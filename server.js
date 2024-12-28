64f96dc9e65a3257740213d98c9b21766ec3584b
edupoly <info.edupoly@gmail.com>
2024-12-28 19:32:21 +0530

    hellopush

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT||3400
 
 app.get("/",(req,res)=>{
-    res.send("hello Hope you like this video, please like share and subscribe")
+    res.send("<h1>Please Like Share and Subscribe... Please</h1>")
 })
 
 app.listen(port,()=>console.log('server running on ',port))
